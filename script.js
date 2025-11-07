25386183fff86c449c4b273259dc340708c6ab5a
ahlam201601 <elhadriahlam9@gmail.com>
2025-11-07 01:27:16 +0100

    js contact

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ function HideSidebar(){
   const sidebar = document.querySelector(".sidebar")
   sidebar.style.display = "none"
 }
+
+const Send = document.getElementById("Send")
+Send.addEventListener("click", function SendMessage(e){
+  e.preventDefault();
+  const name = document.getElementById("message").value
+  const email = document.getElementById("email").value
+  const message = document.getElementById("message").value
+  const champ = document.getElementById("champ")
+  const success = document.getElementById("success")
+  if (name === "" || email === "" || message === "") {
+    champ.textContent = " All fields are required !";
+    champ.style.color = "red";
+  }else{
+    success.textContent = "successfully";
+    success.style.color = "green"
+  }
+
+  if(email.match(/^[a-zA-Z0-9._%+-]+@gmail.com$/)){
+    champ.textContent = ""
+  }else{
+    champ.textContent = "Invalid email address.";
+    champ.style.color = "red";
+  }
+
+})
