c4114c1c9ab0fc4b3cb18fc5683d0a8766f4f162
ahlam201601 <elhadriahlam9@gmail.com>
2025-11-07 16:50:26 +0100

    Menu

--- script.js
+++ script.js
@@ -15,19 +15,40 @@ Send.addEventListener("click", function SendMessage(e){
   const message = document.getElementById("message").value
   const champ = document.getElementById("champ")
   const success = document.getElementById("success")
-  if (name === "" || email === "" || message === "") {
-    champ.textContent = " All fields are required !";
-    champ.style.color = "red";
-  }else{
-    success.textContent = "successfully";
-    success.style.color = "green"
-  }
+  const emailregex = document.getElementById("emailregex")
 
-  if(email.match(/^[a-zA-Z0-9._%+-]+@gmail.com$/)){
-    champ.textContent = ""
-  }else{
-    champ.textContent = "Invalid email address.";
+  // if (name === "" || email === "" || message === "") {
+  //   champ.textContent = " All fields are required !";
+  //   champ.style.color = "red";
+  //   success.textContent = ""
+  // }else if(email.match(/^[a-zA-Z0-9._%+-]+@gmail.com$/)){
+  //   emailregex.textContent = ""
+  // }else{
+  //   emailregex.textContent = "Invalid email address.";
+  //   emailregex.style.color = "red";
+  //   champ.textContent = "";
+  //   success.textContent = ""
+  // }
+
+  // if( name !== "" && email !== "" && message !==""){
+  //   success.textContent = "successfully";
+  //   success.style.color = "green";
+  //   champ.textContent = ""
+  // }
+
+  if (name === "" || email === "" || message === "") {
+    champ.textContent = "All fields are required !";
     champ.style.color = "red";
+    success.textContent = ""
+  } else if (!email.match(/^[a-zA-Z0-9._%+-]+@gmail\.com$/)) {
+    emailregex.textContent = "Invalid email address.";
+    emailregex.style.color = "red";
+    champ.textContent = ""
+    success.textContent = ""
+  } else {
+    success.textContent = "successfully";
+    success.style.color = "green";
+    champ.textContent = ""
   }
 
 })
